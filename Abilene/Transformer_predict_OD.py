e811ad7b0744d5f78ed6faae0b26d8871d85bbb5
PeanutADi <2535044931@qq.com>
2021-09-15 21:04:38 +0800

    positional encoding

--- Abilene/Transformer_predict_OD.py
+++ Abilene/Transformer_predict_OD.py
@@ -36,6 +36,22 @@ class RNN(nn.Module):
         out = self.out(r_out[:, -1, :]) # return the last value
 
         return out
+        
+#### positional encoding ####
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=50):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        # pe.requires_grad = False
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return x + self.pe[:x.size(0), :]
 
 class Transformer(nn.Module):
     def __init__(self, feature_size=512, num_layers=1, dropout=0):
