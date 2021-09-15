c0bacb5ee7162cf4051a8d38daf80f3fb72cb95e
PeanutADi <2535044931@qq.com>
2021-09-15 21:24:38 +0800

    change head num

--- Abilene/Transformer_predict_OD.py
+++ Abilene/Transformer_predict_OD.py
@@ -59,7 +59,7 @@ class Transformer(nn.Module):
         self.model_type = 'Transformer'
         self.src_mask = None
         self.pos_encoder = PositionalEncoding(feature_size) 
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=feature_size, nhead=2, dropout=dropout) 
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=feature_size, nhead=1, dropout=dropout) 
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
         self.decoder = nn.Linear(feature_size, 1) 
         self.init_weights()
